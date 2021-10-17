57adf96a263b00b9059a0c8f0e181b052b70622e
Tony Trinh <tony19@gmail.com>
2021-10-17 01:50:20 -0500

    config Vite to inline js/css in index.html

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { viteSingleFile } from 'vite-plugin-singlefile'
+import { minifyHtml } from 'vite-plugin-html'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    viteSingleFile(),
+    minifyHtml(),
+  ],
+  build: {
+    cssCodeSplit: false,
+    assetsInlineLimit: 100000000,
+    rollupOptions: {
+      output: {
+        manualChunks: () => "everything.js",
+      },
+    },
+  }
 })
