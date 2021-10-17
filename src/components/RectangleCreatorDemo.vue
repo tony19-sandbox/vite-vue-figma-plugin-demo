463b96cc88877fac74c73f0e5bd3117ca05043fb
Tony Trinh <tony19@gmail.com>
2021-10-17 01:51:27 -0500

    add demo from figma plugin example

---
+++ src/components/RectangleCreatorDemo.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const count = ref(0)
+
+const create = () => {
+  parent.postMessage({ pluginMessage: { type: 'create-rectangles', count: count.value } }, '*')
+}
+
+const cancel = () => {
+  parent.postMessage({ pluginMessage: { type: 'cancel' } }, '*')
+}
+</script>
+
+<template>
+  <h2>Rectangle Creator</h2>
+  <p>Count: <input id="count" v-model="count"></p>
+  <button @click="create">Create</button>
+  <button @click="cancel">Cancel</button>
+</template>
